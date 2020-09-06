f0619fd90a7201c198aa4161b39097daf70a6e70
acrylic-style <me@acrylicstyle.xyz>
2020-09-06 17:30:23 +0900

    Conflicts with MCBans (probably will be dropped)

--- src/main/kotlin/pro/banmaster/bukkit/BanMasterPlugin.kt
+++ src/main/kotlin/pro/banmaster/bukkit/BanMasterPlugin.kt
@@ -6,6 +6,12 @@ import pro.banmaster.api.BanMasterAPI
 
 class BanMasterPlugin: BanMasterAPIImpl() {
     override fun onEnable() {
+        conflicts("MCBans")
         Bukkit.getServicesManager().register(BanMasterAPI::class.java, this, this, ServicePriority.Normal)
+    }
+
+    private fun conflicts(plugin: String) {
+        if (Bukkit.getPluginManager().getPlugin(plugin) != null)
+            throw RuntimeException("plugin '$plugin' is incompatible with this plugin. Please remove the plugin and restart the server.")
     }
 }
